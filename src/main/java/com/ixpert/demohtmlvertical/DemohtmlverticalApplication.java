528fe5b092ac5f0e8f66f65f7177d4fb6b485a3d
ixpert <amr.elnagar@outlook.com>
2019-01-06 22:14:36 +0100

    Initial commit

---
+++ src/main/java/com/ixpert/demohtmlvertical/DemohtmlverticalApplication.java
@@ -0,0 +1,14 @@
+package com.ixpert.demohtmlvertical;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemohtmlverticalApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DemohtmlverticalApplication.class, args);
+    }
+
+}
+
