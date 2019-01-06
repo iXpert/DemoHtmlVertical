5c234862957075c3c2c95b728011bf75e42c050d
ixpert <amr.elnagar@outlook.com>
2019-01-06 23:46:38 +0100

    23:45-06012019

--- src/main/java/com/ixpert/demohtmlvertical/controller/DefaultController.java
+++ src/main/java/com/ixpert/demohtmlvertical/controller/DefaultController.java
@@ -12,6 +12,37 @@ public class DefaultController {
     }
 
 
+    @GetMapping(value = "/newAuthor")
+    public String newAuthor(){
+        return "/newAuthor";
+    }
+
+    @GetMapping(value = "/listAuthors")
+    public String listAuthors(){
+        return "/listAuthors";
+    }
+
+    @GetMapping(value = "/listCategories")
+    public String listCategories(){
+        return "/listCategories";
+    }
+
+    @GetMapping(value = "/newCategory")
+    public String newCategory(){
+        return "/newCategory";
+    }
+
+    @GetMapping(value = "/newBook")
+    public String newBook(){
+        return "/newBook";
+    }
+
+    @GetMapping(value = "/listBooks")
+    public String listBooks(){
+        return "/listBooks";
+    }
+
+
 
 
 
